bf0646f7454c26fe627749b21d83b56acc2e925c
Ars-Mik <arsen.0mikailov@ya.ru>
2026-09-20 23:05:51 +0300

    Save  property values after refresh

--- app/public/admin/js/property.js
+++ app/public/admin/js/property.js
@@ -1,6 +1,77 @@
 (function ($) {
     'use strict';
 
+    function collectPropertyValues($properties) {
+        var values = {};
+
+        $properties.find('.name_select_rielt').each(function () {
+            var $field = $(this);
+            var propertyId = $field.attr('data-property-id');
+            var $multiple = $field.find('.checkbox_property');
+
+            if ($multiple.length) {
+                var selectedValues = [];
+
+                $multiple.find('.line_chek').each(function () {
+                    var $choice = $(this);
+                    var $checkbox = $choice.find('input[type="checkbox"]');
+
+                    if ($checkbox.prop('checked')) {
+                        selectedValues[selectedValues.length] =
+                            $choice.find('.ckeck_param').attr('data-val');
+                    }
+                });
+
+                values[propertyId] = selectedValues;
+                return;
+            }
+
+            var $control = $field
+                .find('input.ag_pole_good, select.ag_pole_good')
+                .first();
+
+            if ($control.length) {
+                values[propertyId] = $control.val();
+            }
+        });
+
+        return values;
+    }
+
+    function restorePropertyValues($properties, values) {
+        $properties.find('.name_select_rielt').each(function () {
+            var $field = $(this);
+            var propertyId = $field.attr('data-property-id');
+
+            if (!Object.prototype.hasOwnProperty.call(values, propertyId)) {
+                return;
+            }
+
+            var $multiple = $field.find('.checkbox_property');
+
+            if ($multiple.length) {
+                var selectedValues = values[propertyId];
+
+                $multiple.find('.line_chek').each(function () {
+                    var $choice = $(this);
+                    var answerId = $choice.find('.ckeck_param').attr('data-val');
+
+                    $choice.find('input[type="checkbox"]').prop(
+                        'checked',
+                        $.inArray(answerId, selectedValues) !== -1
+                    );
+                });
+
+                return;
+            }
+
+            $field
+                .find('input.ag_pole_good, select.ag_pole_good')
+                .first()
+                .val(values[propertyId]);
+        });
+    }
+
     // Выбор категории в товаре и обновление блока характеристик.
     $('body').on('change', '.js-category', function () {
         var category = [];
@@ -13,6 +84,8 @@
             category[category.length] = $(this).attr('data-category-id');
         });
 
+        var propertyValues = collectPropertyValues($properties);
+
         $properties.addClass('is-loading').attr('aria-busy', 'true');
 
         $.ajax({
@@ -23,10 +96,13 @@
             success: function (data) {
                 if (data != 'no') {
                     $properties.html(data);
+                    restorePropertyValues($properties, propertyValues);
                 }
             },
             error: function () {
-                $properties.html('<div class="error-state">Не удалось обновить характеристики.</div>');
+                $properties.html(
+                    '<div class="error-state">Не удалось обновить характеристики.</div>'
+                );
             },
             complete: function () {
                 $properties.removeClass('is-loading').attr('aria-busy', 'false');
